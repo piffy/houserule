1dd994c4289bedadcfc35e6781c6b67b5483941a
Marcello Missiroli <prof.missiroli@gmail.com>
2012-05-02 18:01:56 +0200

    User authentication (NOT WORKING)

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,22 @@ class User < ActiveRecord::Base
 
 
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
+
+  # Checks if password is OK
+  # Return same object unless check fails
+  def authenticate(pw)
+    if self.password==pw
+      self
+    else
+      false
+    end
+
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
