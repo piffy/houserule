0ab1f00394e16d5c00d44844ba06c5af4b355a75
Marcello Missiroli <prof.missiroli@gmail.com>
2012-05-17 19:49:00 +0200

    (dev) event_step.rb

---
+++ features/step_definitions/event_step.rb
@@ -0,0 +1,10 @@
+Given /^che esistono i seguenti eventi dell'utente "([^"]*)":$/ do |user_email, event_table|
+
+  user = User.find_by_email(user_email)
+
+  event_table.hashes.each do |event|
+    ev=user.events.build(event)
+    ev.save!
+  end
+
+end
