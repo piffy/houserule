ee2dcd0c7b23025906ddae186ae78ea865edc6b2
Marcello Missiroli <prof.missiroli@gmail.com>
2012-06-29 19:38:45 +0200

    Added gravatar, modified user listing

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+    #Returns the Gravatar (http://gravatar.com/) for the given user Defaults to identicon.
+    def gravatar_for(user, size=80)
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?&d=identicon&s=#{size}"
+      image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
+
 end
