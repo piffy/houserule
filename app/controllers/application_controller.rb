3d949681408d2b8dfcb1bb0aefb14522cb9e620e
Marcello Missiroli <prof.missiroli@gmail.com>
2012-09-13 17:36:43 +0200

    Change URL, expanded help, minor fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,14 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   def self.version
-    '0.2.3'
+    '0.2.4'
   end
 
   #Added in order to have working links in development environment
   def self.hostname
     if Rails.env == 'production'
-      'afternoon-spring-6294.herokuapp.com'
+      #'afternoon-spring-6294.herokuapp.com'
+      'www.houserule.it'
     else
       'localhost:3000'
     end
